383ad946d5f3970b339662a6f8ff762c1e69dbce
TeBenachi <TeBenachi@gmail.com>
2020-05-26 09:20:55 -0400

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,3 +1,15 @@
+//////////
+
+(1) Password require to view comments
+(2) Comment form ( If not logged in, show Name + Email + URL)
+(3) LOOP if have comments ==> foreach( $comments as $comment ){} 
+  a. Total number of comments
+  b. Avatar
+  c. Author
+  d. Date
+  e. Comment text
+//////////
+
 <?php 
 
 if( post_password_required($post) ) {
